7a5932d5b9c4c4fed108565870c9e0a5f0bab3e2
DYTye <adityaarrofi806@gmail.com>
2026-04-28 00:28:45 +0700

    first commit: full project

---
+++ src/main.js
@@ -0,0 +1,60 @@
+// main.js
+import "./style.css";
+import navbar from "./header.html?raw";
+import footer from "./footer.html?raw";
+import main from "./landing.html?raw";
+import project from "./project.html?raw";
+import art from "./art.html?raw";
+
+document.getElementById("navbar-placeholder").innerHTML = navbar;
+document.getElementById("footer-placeholder").innerHTML = footer;
+document.getElementById("main").innerHTML = main;
+const navItems = document.querySelectorAll(".nav-items");
+navItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    navItems.forEach((x) => {
+      x.classList.remove(
+        "bg-amber-100",
+        "text-black",
+        "backdrop-blur-sm",
+        "rounded-xl",
+      );
+    });
+
+    item.classList.add(
+      "bg-amber-100",
+      "text-black",
+      "backdrop-blur-sm",
+      "rounded-xl",
+    );
+  });
+});
+
+const btnHome = document.getElementById("lofi");
+const btnProject = document.getElementById("project");
+const btnArt = document.getElementById("art");
+if (btnProject) {
+  btnProject.addEventListener("click", () => {
+    document.getElementById("main").innerHTML = project;
+  });
+}
+if (btnArt) {
+  btnArt.addEventListener("click", () => {
+    document.getElementById("main").innerHTML = art;
+  });
+}
+if (btnHome) {
+  btnHome.addEventListener("click", () => {
+    document.getElementById("main").innerHTML = main;
+
+    navItems.forEach((x) => {
+      x.classList.remove(
+        "bg-amber-100",
+        "text-black",
+        "backdrop-blur-sm",
+        "rounded-xl",
+      );
+    });
+  });
+} else {
+}
