99bca0cae9bd3588fb88c5de8714d0f6e5d00197
DYTye <adityaarrofi806@gmail.com>
2026-04-28 21:37:49 +0700

    mobile botnavbar

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import footer from "./footer.html?raw";
 import main from "./landing.html?raw";
 import project from "./project.html?raw";
 import art from "./art.html?raw";
+import botNav from "./bottomNav.html?raw";
 
 document.getElementById("navbar-placeholder").innerHTML = navbar;
+document.getElementById("botNav").innerHTML = botNav;
 document.getElementById("footer-placeholder").innerHTML = footer;
 document.getElementById("main").innerHTML = main;
 const navItems = document.querySelectorAll(".nav-items");
