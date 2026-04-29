1eb6c7f2b07464771817439207f550999defbea7
DYTye <adityaarrofi806@gmail.com>
2026-04-30 00:36:51 +0700

    animasi desktop for navbar

--- src/main.js
+++ src/main.js
@@ -11,26 +11,63 @@ document.getElementById("navbar-placeholder").innerHTML = navbar;
 document.getElementById("botNav").innerHTML = botNav;
 document.getElementById("footer-placeholder").innerHTML = footer;
 document.getElementById("main").innerHTML = main;
-const navItems = document.querySelectorAll(".nav-items");
-navItems.forEach((item) => {
-  item.addEventListener("click", () => {
-    navItems.forEach((x) => {
-      x.classList.remove(
-        "bg-amber-100",
-        "text-black",
-        "backdrop-blur-sm",
-        "rounded-xl",
-      );
-    });
 
-    item.classList.add(
-      "bg-amber-100",
-      "text-black",
-      "backdrop-blur-sm",
-      "rounded-xl",
-    );
+const navItems = document.querySelectorAll(".nav-items");
+const bg = document.getElementById("lofi");
+
+navItems.forEach((nav) => {
+  nav.addEventListener("click", () => {
+    const posisiBg = nav.getBoundingClientRect();
+    const posisiParent = nav.parentElement.getBoundingClientRect();
+
+    bg.style.width = posisiBg.width + "px";
+    bg.style.height = posisiBg.height + "px";
+
+    bg.style.left = posisiBg.left - posisiParent.left + "px";
+    bg.style.top = posisiBg.top - posisiParent.top + "px";
   });
 });
+
+if (navItems.length > 0) {
+  setTimeout(() => {
+    const nav = navItems[0]; 
+
+    const posisiBg = nav.getBoundingClientRect();
+    const posisiParent = nav.parentElement.getBoundingClientRect();
+
+    bg.style.width = posisiBg.width + "px";
+    bg.style.height = posisiBg.height + "px";
+    bg.style.left = posisiBg.left - posisiParent.left + "px";
+    bg.style.top = posisiBg.top - posisiParent.top + "px";
+  }, 50);
+}
+
+// navItems.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     navItems.forEach((x) => {
+//       x.classList.remove(
+//         "bg-amber-100",
+//         "text-black",
+//         "backdrop-blur-sm",
+//         "rounded-xl",
+//       );
+//     });
+
+//     item.classList.add(
+//       "bg-amber-100",
+//       "text-black",
+//       "backdrop-blur-sm",
+//       "rounded-xl",
+//     );
+//   });
+// });
+
+// const anim = document.getElementById("anim");
+// const bg = anim.querySelector("span");
+
+// anim.addEventListener("click", () => {
+//  bg.classList.toggle("translate-x-0")
+// });
 
 const btnHome = document.querySelectorAll(".lofi");
 const btnProject = document.querySelectorAll(".project");
@@ -72,12 +109,25 @@ btnProject.forEach((btn) => {
       rmBebek.classList.add("hidden");
     }
 
-    const addBg = document.getElementById("body");
-    if (addBg) {
-      addBg.classList.remove(
-        "bg-[url('https://txwatpcjenskrdnispuu.supabase.co/storage/v1/object/public/pasar3d/bgc.webp')]",
-      );
-      addBg.classList.add("bg-[#212326]");
+    const rmBg = document.getElementById("footer-placeholder");
+    if (rmBg) {
+      rmBg.classList.add("w-fit", "mx-auto", "rounded-md");
+    }
+
+    const slide = document.getElementById("slide");
+    if (slide) {
+      const foto = [];
+      foto.push("bgc.png", "kupi.png", "profil.png");
+
+      let i = 0;
+      setInterval(() => {
+        slide.src = foto[i];
+        i++;
+
+        if (i === foto.length) {
+          i = 0;
+        }
+      }, 1000);
     }
   });
 });
@@ -133,3 +183,12 @@ btnSource.forEach((y) => {
 //   });
 // } else {
 // }
+
+const foto = [];
+foto.push("bgc.png", "kupi.png", "profil.png");
+
+let i = 0;
+setInterval(() => {
+  foto.src = "foto[i]";
+  i++;
+}, 1000);
